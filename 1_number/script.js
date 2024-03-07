800fa0ecccab6bfa769281eb7e7fb50784e46699
higor henrique da silva mastrangelo <126088507+higorhenriquesilva@users.noreply.github.com>
2024-03-06 21:06:40 -0300

    Add files via upload

---
+++ 1_number/script.js
@@ -0,0 +1,16 @@
+//tipo number
+
+const meuNumber = 24;
+
+const telefone = 44999227567
+
+const idade = 29;
+
+
+console.log(meuNumero);
+console.log(telefone);
+console.log(idade);
+
+
+const soma = meuNumero + idade;
+console.log(soma)
---
+++ 2_string/script.js
@@ -0,0 +1,16 @@
+const text1 = "vem bb";
+const texto2 = 'fja';
+
+const texto3 = 'laaa eleee foi'
+
+console.log(texto1);
+console.log(texto2);
+console.log(texto3);
+
+console.log(texto2 + texto3)
+
+const cifrao = '1\u0024';
+const aMaisculo='\u0041';
+
+console.log(cifrao);
+console.log(aMaisculo);
